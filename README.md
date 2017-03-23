3fca61b1bee1d55031edf5f0f9cce86e9ba3808d
ympbyc <ympbyc@gmail.com>
2017-03-23 23:27:07 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,82 @@
+ISD1700 Fragmented Memory Manager
+=================================
+
+Arduino sketch that controls ISD1700 record/playback ic over SPI. It's packed with awesome features. Read on.
+
+<!-- ![Test Setup](docs/test-setup.jpg?raw=true "Test Setup") -->
+<img src="docs/test-setup.jpg?raw=true" alt="Test Setup" title="Test Setup" width="480" style="max-width:100%;">
+
+Caution
+-------
+
+It's a WIP and a little buggy.
+
+Features
+--------
+
++ Fragmented memory management: divides the memory into N fixed-size randomly-accessible sections.
++ LED indicators: N LEDs can be attached to IO pins to indicate memory occupancy status and cursor position.
++ Multi-function buttons: Reduce the number of pushbuttons by utilizing single-click/double-click/long-press actions.
++ Sleep mode: Arduino enters SLEEP_MODE_PWR_DOWN after putting ISD1700 to power-down mode. Wakes up on button action and immediately executes the given command.
+
+\* N being an integer
+
+Default Setup
+-------------
+
+### Memory management
+
+Address range is configured for ISD1790. Adjust LAST_ADDR macro value to match your chip. (Refer to ISD1700 Design Guide)
+
+Number of sections the memory gets devided into is set by SLOT_NUM macro.
+
+### APC register
+
+By default the APC is setup to accept input from Mic. Change the APC macro value to match your need. (Refer to ISD1700 Design Guide)
+
+### Push Buttons
+
+#### MAIN_BTN
+
++ single-click: (play) play current section
++ double-click: (fwd)  move to next occupied section and play
++ long-press:   (rec)  start recording into first vacant section
+
+#### SUB_BTN
+
++ single-click: erase current section
++ long-press:   erase entire memory
+
+### LEDs
+
++ ON when the section is occupied
++ OFF when the section is vacant
++ Blink to indicate current section (play, fwd, rec)
+
+Default Wiring
+--------------
+
+| ISD1700 | Arduino |
+| ------- | ------- |
+| MISO    | D12     |
+| MOSI    | D11     |
+| SCLK    | D13     |
+| SS      | D10     |
+
+
+| Arduino | LED/BTN  |
+| ------- | -------- |
+| D14(A0) | LED0     |
+| D15(A1) | LED1     |
+| ...     | LEDN-1   |
+| D3      | MAIN_BTN |
+| D2      | SUB_BTN  |
+
+
+Schematic
+---------
+
+<!-- ![ISD1700 Schematic](docs/schematic-isd1700.jpg?raw=true "ISD1700 Schematic") -->
+<img src="docs/schematic-isd1700.jpg?raw=true" alt="ISD1700 Schematic" title="ISD1700 Schematic" width="480" style="max-width:100%;">
+<!-- ![Arduino Schematic](docs/schematic-arduino.jpg?raw=true "Arduino Schematic") -->
+<img src="docs/schematic-arduino.jpg?raw=true" alt="Arduino Schematic" title="Arduino Schematic" width="480" style="max-width:100%;">
---
+++ sticky-watch.ino
@@ -0,0 +1,376 @@
+/*
+ * ISD1700 Manager sketch
+ * Author: Minori Yamashita
+ * Contact: ympbyc@gmail.com
+ * 
+ * Thanks! to following library authors:
+ * ISD1700 lib by neuron_upheaval https://forum.arduino.cc/index.php?topic=52509.0
+ * Timer lib by Simon Monk http://playground.arduino.cc/Code/Timer
+ * OneButton lib by Matthias Hertel http://www.mathertel.de/Arduino/OneButtonLibrary.aspx
+ */
+
+#include <avr/interrupt.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include <avr/io.h>
+#include <prescaler.h>
+#include <EEPROM.h>
+#include <ISD1700.h>
+#include <OneButton.h>
+#include <Timer.h>
+
+#define SS 10
+#define FIRST_ADDR 0x010
+#define LAST_ADDR 0x2df
+#define APC 0xE1 //000011100001
+#define SLOT_NUM 5
+
+#define MAIN_BTN 3
+#define SUB_BTN  2
+
+#define LEDI 14 //1 for D1, 14 for A0
+#define LEDN 5
+#define LEDFREQ 100
+#define LEDREP 5
+
+#define REC_TIMEUP ((90 * 1000) / SLOT_NUM)
+#define AVR_PWR_DWN_TIME 3000
+
+int digitalPinToInterrupt(int d) {
+  if (d == 2) return 0;
+  if (d == 3) return 1;
+  return -1;
+}
+
+int interrupt_main_btn = digitalPinToInterrupt(MAIN_BTN);
+int interrupt_sub_btn = digitalPinToInterrupt(SUB_BTN);
+
+ISD1700 chip(SS); // Initialize chipcorder with
+                 // SS at Arduino's digital pin 10
+
+OneButton btn(SUB_BTN, true);
+OneButton btn2(MAIN_BTN, true);
+
+Timer tim;
+int tim_id;
+int c_w_t_id;
+int r_tu_t_id;
+int avr_pd_t_id;
+
+uint16_t apc = 0;
+
+uint16_t slot_addrs[SLOT_NUM];
+uint16_t end_addrs[SLOT_NUM];
+int cur_slot = 0;
+
+bool recording = false;
+
+void setup() {
+  int i;
+  analogReference(INTERNAL);
+  Serial.begin(115200);
+  /* lower power consumption */
+  DDRD &= B00000011;       // set Arduino pins 2 to 7 as inputs
+  DDRB = B00000000;        // set pins 8 to 13 as inputs
+  PORTD |= B11111100;      // enable pullups on pins 2 to 7
+  PORTB |= B11111111;      // enable pullups on pins 8 to 13
+  setClockPrescaler(CLOCK_PRESCALER_1); 
+  
+  // re-set pin-mode for SPI after above setting
+  pinMode(SCK_PIN, OUTPUT);
+  pinMode(MOSI_PIN, OUTPUT);
+  pinMode(MISO_PIN, INPUT);
+  pinMode(SS, OUTPUT);
+
+  for (i=LEDI; i<LEDI+LEDN; i++)
+    pinMode(i, OUTPUT);
+  pinMode(MAIN_BTN, INPUT_PULLUP);
+  pinMode(SUB_BTN, INPUT_PULLUP);
+
+  btn.setClickTicks(300);
+  btn.setPressTicks(400);
+  btn2.setClickTicks(300);
+  btn2.setPressTicks(1500); //press longer for erase
+  btn.attachClick(playback);
+  btn.attachLongPressStart(rec);
+  btn.attachLongPressStop(rec_end);
+  btn2.attachClick(forward);
+  btn2.attachLongPressStop(erase_one);
+  digitalWrite(SS, HIGH); // just to ensure
+  while (!chip_wakeup())
+    delay(10);
+  if (chip.PU()) {
+    Serial.print(chip.CMD_ERR());
+    Serial.print(chip.RDY());
+    Serial.println("Chip woke up!");
+  }
+  apc |= APC;
+  chip.wr_apc2(apc);
+  check_chip_err();
+
+  //erase
+  
+  /*chip.g_erase();
+  for (i=0; i<SLOT_NUM; i++)
+    EEPROM[i] = 0;
+  check_chip_err();*/
+  
+  led_update();
+
+  //setup memory management
+  for (i=0; i<SLOT_NUM; i++) {
+    slot_addrs[i] = floor((LAST_ADDR - FIRST_ADDR) / SLOT_NUM) * i + FIRST_ADDR + i;
+    end_addrs[i] = floor((LAST_ADDR - FIRST_ADDR) / SLOT_NUM) * (i+1) + FIRST_ADDR + i;
+    Serial.println(slot_addrs[i], HEX);
+  }
+
+  chip.pd();
+  avr_sleep();
+}
+
+bool chip_wakeup () {
+  // Power Up
+  tim.stop(avr_pd_t_id); //clear pwrdwn timeout
+  Serial.println("ISD1700 wakeup");
+  chip.rd_status();
+  if (chip.CMD_ERR()) { Serial.println("rd_status CMD_ERR"); }
+  if (! chip.PU()) { Serial.println("instructing pu");  chip.pu();}
+  delay(10);
+  chip.stop(); //stop whatever it's doing
+  chip.clr_int();
+  if (chip.RDY()) return true;
+  chip.reset();
+  delay(100);
+  if (chip.RDY()) return true;
+  led_notwakeup_pattern();
+  return false;
+}
+
+bool chip_cleanup () {
+  tim.stop(c_w_t_id);
+  Serial.print("Status---> ");
+  Serial.print(chip.CMD_ERR()? "CMD_ERR ": " ");
+  Serial.print(chip.PU()? "PU ": " ");
+  Serial.print(chip.RDY()? "RDY ": "Not_RDY");
+  Serial.println();
+  c_w_t_id = tim.every(1000, attempt_pd);
+}
+
+void attempt_pd () {
+  chip.rd_status();
+  if (chip.INT() || chip.EOM()) {
+    tim.stop(c_w_t_id);
+    //chip.clr_int();
+    chip.pd();
+    Serial.println(chip.CMD_ERR() ? "pd CMD_ERR" : "ISD1700 powerdown");
+    avr_pd_t_id = tim.after(AVR_PWR_DWN_TIME, avr_sleep);
+  }
+}
+
+void playback () {
+  Serial.println("button action: playback");
+  if ( ! EEPROM[cur_slot]) return;
+  if ( ! chip_wakeup()) return;
+  chip.set_play(slot_addrs[cur_slot], end_addrs[cur_slot]);
+  if (check_chip_err()) return;
+  tim.stop(tim_id);
+  tim_id = tim.oscillate(LEDI+cur_slot, LEDFREQ, HIGH, LEDREP);
+  chip_cleanup();
+}
+
+void forward () {
+  Serial.println("button action: forward");
+  if ( ! chip_wakeup()) return;
+  chip.stop();
+  //chip.fwd();
+  cur_slot = next_occupied_slot(cur_slot);
+  chip.set_play(slot_addrs[cur_slot], end_addrs[cur_slot]);
+  if (check_chip_err()) return;
+  tim.stop(tim_id);
+  tim_id = tim.oscillate(LEDI+cur_slot, LEDFREQ, HIGH, LEDREP);
+  chip_cleanup();
+}
+
+void rec () {
+  Serial.println("button action: rec");
+  int next_slot;
+  if ( ! chip_wakeup()) return;
+  next_slot = next_available_slot(cur_slot);
+  if (next_slot < 0) return;
+  recording = true;
+  chip.set_rec(slot_addrs[next_slot], end_addrs[next_slot]);
+  tim.stop(tim_id);
+  tim_id = tim.oscillate(LEDI+next_slot, LEDFREQ, HIGH);
+  EEPROM.write(next_slot, 1);
+  cur_slot = next_slot;
+  r_tu_t_id = tim.after(REC_TIMEUP, rec_end); //ensure rec_end gets called
+}
+
+void rec_end () {
+  if (! recording) return;
+  recording = false;
+  Serial.println("rec end");
+  tim.stop(tim_id);
+  tim.stop(r_tu_t_id);
+  delay(10);
+  if (check_chip_err()) return;
+  chip.stop();
+  led_update();
+  chip_cleanup();
+}
+
+void erase_one () {
+  if ( ! chip_wakeup()) return;
+  chip.set_erase(slot_addrs[cur_slot], end_addrs[cur_slot]);
+  delay(10);
+  if (check_chip_err()) return;
+  EEPROM.write(cur_slot, 0);
+  led_update();
+  cur_slot = next_occupied_slot(cur_slot);
+  chip_cleanup();
+}
+
+void erase_all () {
+  int i;
+  if ( ! chip_wakeup()) return;
+  chip.g_erase();
+  delay(10);
+  if (check_chip_err()) return;
+  for (i=0; i<SLOT_NUM; i++)
+    EEPROM[i] = 0;
+  led_update();
+  cur_slot = 0;
+  chip_cleanup();
+}
+
+void led_update () {
+  int i;
+  tim.stop(tim_id);
+  //led display update
+  for (i=0; i<SLOT_NUM; i++) {
+    if (EEPROM[i])
+      digitalWrite(LEDI+i, HIGH);
+    else
+      digitalWrite(LEDI+i, LOW);
+  }
+}
+
+bool check_chip_err () {
+  if (! chip.CMD_ERR()) return false;
+  error_pattern();
+  return true;
+}
+
+void led_off () {
+  int i;
+  for (i=0; i<SLOT_NUM; i++)
+    digitalWrite(LEDI+i, LOW);
+}
+
+void error_pattern () {
+  int i;
+  for (i=0; i<SLOT_NUM; i++)
+    digitalWrite(LEDI+i, LOW);
+  for (i=0; i<SLOT_NUM; i++) {
+    digitalWrite(LEDI+i, HIGH);
+    delay(100);
+  }
+  for (i=SLOT_NUM-1; i>-1; i--) {
+    digitalWrite(LEDI+i, LOW);
+    delay(100);
+  }
+  led_update();
+}
+
+void led_notwakeup_pattern () {
+  for (int i = 0; i < 3; i++)
+    error_pattern();
+  led_update();
+}
+
+int next_available_slot (int cur) {
+  for (int i=0; i<SLOT_NUM; i++) {
+    if ( ! EEPROM[i]) return i;
+  }
+  return -1;
+}
+
+int next_occupied_slot (int cur) {
+  int nxt = (cur + 1) % SLOT_NUM;
+  for (int i=nxt; i<SLOT_NUM+nxt; i++) {
+    if (EEPROM[i % SLOT_NUM]) return i % SLOT_NUM;
+  }
+  return 0;
+}
+
+void pinInterrupt () {
+  sleep_disable(); //Wait for an interrupt, disable, and continue
+  detachInterrupt(interrupt_main_btn);
+  detachInterrupt(interrupt_sub_btn);
+}
+
+void avr_sleep () {
+  Serial.println("avr_sleep reached");
+  if (digitalRead(MAIN_BTN) == LOW || digitalRead(SUB_BTN) == LOW) return;
+  sleep_enable();  //Set sleep enable (SE) bit
+  attachInterrupt(interrupt_main_btn, pinInterrupt, LOW);
+  attachInterrupt(interrupt_sub_btn,  pinInterrupt, LOW);
+  delay(100);
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  led_off();
+  Serial.println("Arduino PWR_DOWN");
+  delay(10);
+  sleep_mode();    //Put the device to sleep
+  
+  sleep_disable(); //Wait for an interrupt, disable, and continue
+  Serial.println("Arduino Wakeup");
+  led_update();
+}
+
+/* 
+ * Secret Arduino Voltmeter – Measure Battery Voltage 
+ * https://provideyourown.com/2012/secret-arduino-voltmeter-measure-battery-voltage/
+ */
+long readVcc() {
+  // Read 1.1V reference against AVcc
+  // set the reference to Vcc and the measurement to the internal 1.1V reference
+  #if defined(__AVR_ATmega32U4__) || defined(__AVR_ATmega1280__) || defined(__AVR_ATmega2560__)
+    ADMUX = _BV(REFS0) | _BV(MUX4) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #elif defined (__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__)
+    ADMUX = _BV(MUX5) | _BV(MUX0);
+  #elif defined (__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)
+    ADMUX = _BV(MUX3) | _BV(MUX2);
+  #else
+    ADMUX = _BV(REFS0) | _BV(MUX3) | _BV(MUX2) | _BV(MUX1);
+  #endif  
+
+  delay(2); // Wait for Vref to settle
+  ADCSRA |= _BV(ADSC); // Start conversion
+  while (bit_is_set(ADCSRA,ADSC)); // measuring
+
+  uint8_t low  = ADCL; // must read ADCL first - it then locks ADCH  
+  uint8_t high = ADCH; // unlocks both
+
+  long result = (high<<8) | low;
+
+  result = 1125300L / result; // Calculate Vcc (in mV); 1125300 = 1.1*1023*1000
+  return result; // Vcc in millivolts
+}
+
+//monitor vcc for LiPo protection
+void pd_if_low_battery () {
+  if (readVcc() < 2900) {
+    chip.pd();
+    delay(10);
+    avr_sleep();
+  }
+}
+
+void loop() {
+  pd_if_low_battery(); 
+  btn.tick();
+  btn2.tick();
+  tim.update();
+  delay(10);
+}
+
